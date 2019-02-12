018182d93cb6cba2bccb64ff233d34f6cae8e6db
Vidisha Sethi <43785947+vidi-vi@users.noreply.github.com>
2019-02-12 20:35:03 +0530

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php require_once 'secure.php';?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Welcome to Admin Area</h1>
+        <hr>
+        <a href="logout.php">Logout</a>
+    </body>
+</html>
